6099b9139f2446979e4fffe29895c4d64b601960
TERADA-DANTE <terada.dante@gmail.com>
2020-11-27 00:21:59 +0900

    Set Image fallback

--- mandarine/src/components/album/Gallery.js
+++ mandarine/src/components/album/Gallery.js
@@ -1,10 +1,10 @@
 import '../../styles/Gallery.css'
-
+import defaultImg from '../../assets/images/default.jpg'
 function Gallery({ props }) {
     const [name, img] = props
     return <article className={'Gallery'}>hi This is Galllery
         {name}
-        <img src={img} alt={name} />
+        <img src={img || defaultImg} alt={name} />
     </article>
 }
 
