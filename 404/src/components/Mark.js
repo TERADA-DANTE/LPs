4f5bde399be2c073780532a99f249212ca5d5bc2
TERADA-DANTE <terada.dante@gmail.com>
2020-11-28 18:57:08 +0900

    Complete mark structure

--- 404/src/components/Mark.js
+++ 404/src/components/Mark.js
@@ -1,5 +1,12 @@
+import '../styles/Mark.css'
+
+// import files
+
 function Mark() {
-    return <div> Hi this is mark</div>
+    return <div className={`Mark`}>
+        <div className={`Mark-main`}>This is main</div>
+        <div className={`Mark-aside`}>Aside</div>
+    </div>
 }
 
 export default Mark
