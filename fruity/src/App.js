81c813b58d4cd2883c1c55565d9343b1f6154531
TERADA-DANTE <terada.dante@gmail.com>
2020-11-30 15:12:13 +0900

    Complete main view with object-fit

--- fruity/src/App.js
+++ fruity/src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css'
 import Gallery from './components/Gallery'
 import Cart from './components/Cart'
+// import files
 
 function App() {
   return (
     <div className="App">
+
       <Gallery />
       <Cart />
     </div>
--- fruity/src/components/Gallery.js
+++ fruity/src/components/Gallery.js
@@ -1,16 +1,22 @@
+import { Fragment } from 'react'
 //import css
 import '../styles/Gallery.css'
 
 import Header from './Header/Header'
-import Show from './Gallery/Show'
+// import Show from './Gallery/Show'
 import Language from './Gallery/Language'
-
+import DefaultImg from '../assets/images/default.jpg'
 function Gallery() {
-    return <section className={`Gallery`}>
-        <Header />
-        <Show />
-        <Language />
-    </section>
+    return <Fragment>
+
+        <section className={`Gallery`}>
+            <img src={DefaultImg} alt="default" className={`background`} />
+            <Header />
+            {/* <Show /> */}
+            <Language />
+        </section>
+    </Fragment>
+
 }
 
 export default Gallery
