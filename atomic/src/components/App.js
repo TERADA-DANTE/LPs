1feee2caccf84408c4b9cd1911e7875bbe56c5aa
TERADA-DANTE <terada.dante@gmail.com>
2020-12-12 13:34:47 +0900

    Create .env
    - Create absolute path

--- atomic/src/components/App.js
+++ atomic/src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import { ThemeProvider } from 'styled-components'
-
+import { HomePage } from './pages/'
 import theme from './themes/default'
 function App() {
   return <ThemeProvider theme={theme}>hi</ThemeProvider>
---
+++ atomic/src/components/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// import {
+//   PageTemplate, Header, Hero, Footer, FeatureList,
+// } from 'components'
+
+const HomePage = () => {
+  return <div>hi</div>
+}
+
+export default HomePage
