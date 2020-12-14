24e91f27db5a27af794d797f2d347b8b747f1c09
TERADA-DANTE <terada.dante@gmail.com>
2020-12-14 16:17:19 +0900

    Install fullpage-react

--- library/src/App.js
+++ library/src/App.js
@@ -1,6 +1,19 @@
 import { useEffect } from "react";
 import "./App.css";
-import fullpage from "fullpage.js";
+import { Fullpage, Slide, HorizontalSlider } from "fullpage-react";
+
+const fullPageOptions = {
+  // for mouse/wheel events
+  // represents the level of force required to generate a slide change on non-mobile, 10 is default
+  scrollSensitivity: 7,
+
+  // for touchStart/touchEnd/mobile scrolling
+  // represents the level of force required to generate a slide change on mobile, 10 is default
+  touchSensitivity: 7,
+  scrollSpeed: 500,
+  hideScrollBars: true,
+  enableArrowKeys: true,
+};
 const App = () => {
   useEffect(() => {
     const $ = (selector) => document.querySelector(selector);
