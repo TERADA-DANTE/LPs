923e501676256819f4be1b8ad054c908bdecc23b
TERADA-DANTE <terada.dante@gmail.com>
2020-12-15 14:03:20 +0900

    Install npm craco-less

--- library/src/index.js
+++ library/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import "antd/dist/antd.css";
+import "./App.less";
 
 ReactDOM.render(
   <React.StrictMode>
